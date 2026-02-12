6b86d4a39369e9db6a71a4e3297c9feb5bd59181
Jesús González <jrgonzalez@wyndhamconcorde.com>
2026-02-12 10:32:13 -0400

    feat: cli arguments and requirements

--- main.py
+++ main.py
@@ -1,116 +1,49 @@
-import itertools
-from pprint import pprint
-from typing import List
 import pdfplumber
-from pdfplumber.page import Page
 from report_page import ReportPage
 import report_page
+from dataclasses import dataclass
 
-def test_filter(obj):
-    if(obj['object_type'] == 'char'):
-        if obj['text'] == '' or obj['text'] == ' ':
-            return False
-    return True
+from page_operations import get_report_page, get_tables
+import argparse
 
-type GetTablesReturn = list[list[List[str | None]]]
+@dataclass
+class CliArguments:
+    filename: str
+    pages_to_process: list[int] | None
 
+def setup_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        prog="Wyndham Concorde Payroll Analyzer",
+        description="Parses a PDF file containing payroll data exported from the internal system"
+    )
 
-def get_tables(page: Page) -> GetTablesReturn:
-        # Table processing
-        base_char = page.chars[0] # Every character here is the same size so it doesn't really matter which
-        # Manually determine the right and left boundary for the table
-        rightmost_line:int = max(c['x1'] for c in page.chars)
-        leftmost_line: int = min(c['x1'] for c in page.chars)
-        # Explicitly define vertical lines in order to deal with the weirdness that comes with a text table
-        code_line = leftmost_line + (base_char['width'] * 4.5)
-        balance_line = rightmost_line - (base_char['width'] * 10)
-        deduction_line = balance_line - (base_char['width'] * 18)
-        assignment_line = deduction_line - (base_char['width'] * 20)
-        wages_line = assignment_line - (base_char['width'] * 22)
-        value_line = wages_line - (base_char['width'] * 16)
-        factor_line = value_line - (base_char['width'] * 26)
-        quantity_line = factor_line - (base_char['width'] * 14)
+    parser.add_argument("filename")
 
-        table_settings = {
-            'vertical_strategy': 'explicit',
-            'horizontal_strategy': 'text',
-            "explicit_vertical_lines": [leftmost_line, rightmost_line, code_line, balance_line, deduction_line, assignment_line, wages_line, value_line, factor_line, quantity_line],
-            "min_words_horizontal":3,
-            "snap_x_tolerance": 20,
-            "snap_y_tolerance":7,
-        }
+    parser.add_argument(
+        "--pages",
+        nargs="*",
+        type=int,
+        help="Array (list) of pages to be processed. Defaults to all pages"
+    )
 
-        # Cropping the page for table extraction
-        crop_width = float(page.width)
-        crop_height = float(page.height)
-        cropped_page = page.crop(
-            (
-                #x0
-                0 * crop_width,
-                #top
-                0.28 * crop_height,
-                #x1
-                1 * crop_width,
-                #bottom
-                1 * crop_height
-            )
-        )
+    return parser
 
-        filtered_page = cropped_page.filter(test_filter)
-        # im = filtered_page.to_image()
-        # debug_image = im.reset().debug_tablefinder(table_settings)
-        # debug_image.show() 
-        tables = filtered_page.find_tables(table_settings)
-        separated_tables: GetTablesReturn = []
-        for table in tables:
-            slice_indices = [0]
-            row_index = 0
-            columns_of_interest_index = (1,2,3, 4) 
-            extracted_table = table.extract()
-            # print(extracted_table)
-            for row in extracted_table: 
-                # print(row)
-                string_of_interest = 'TOTAL'
-                row_max_index = len(row) - 1
-                text_options = [row[i] if i <= row_max_index else None for i in columns_of_interest_index]
-                option_contains_string_of_interest = [True for opt in text_options if opt is not None and opt.find(string_of_interest) != -1]
-                found_string = any(result is True for result in option_contains_string_of_interest)
-                # print(text)
-                if(found_string):
-                    # We found the end of a section, now we have to determine whether there are more, or this is the last one.
-                    if(row_index == len(extracted_table) - 1):
-                        slice_indices.append(row_index)
-                    else:
-                        slice_indices.append(row_index + 2)
-                row_index = row_index + 1    
-            sliding_initial_point = 0
-            # print(slice_indices)
-            for index in slice_indices:
-                if(index == 0):
-                    continue;
-                sliced_table = extracted_table[sliding_initial_point:index]
+def parse_cli_arguments(parser: argparse.ArgumentParser):
 
-                sliding_initial_point = index  
-                # Discard any erroneous tables that might have been created by accident
-                # i.e.: An orphaned totals table, which we don't care about as it's not related to a single employee    
-                first_row = sliced_table[0]
-                # print(first_row)
-                if(first_row[0] == None or first_row[0] == ' ' or first_row[0] == ''):
-                    continue
-                separated_tables.append(sliced_table)
+    args = parser.parse_args()
 
-        return separated_tables
-
-def get_report_page(page: Page) -> ReportPage:
-    text = page.extract_text()
-        
-    report_page = ReportPage(text)
-    return report_page
-
+    return CliArguments(
+        filename=args.filename,
+        pages_to_process=args.pages
+    ) 
 
 if __name__ == '__main__':
     processed_pages: list[ReportPage] =[]
-    with pdfplumber.open(path_or_fp='detalle.pdf') as pdf:
+
+    parser = setup_parser()
+    arguments = parse_cli_arguments(parser)
+
+    with pdfplumber.open(path_or_fp=arguments.filename, pages=arguments.pages_to_process) as pdf:
             # for page in pdf.pages:
         for i in range(0,len(pdf.pages)):
             page = pdf.pages[i]
